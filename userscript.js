5d80910efbcf09e805677baa2cd2e7d9f220a2cf
どっかのネット民 <115701341+netminn012@users.noreply.github.com>
2024-03-04 20:51:56 +0900

    Update userscript.js

---
+++ userscript.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Remove Ruby
+// @namespace   http://tampermonkey.net/
+// @version     1.0
+// @description Remove ruby tags in Aozora Bunko
+// @author      Your Name
+// @match       https://www.aozora.gr.jp/*
+// @grant       none
+// @icon　　　　　http://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.aozora.gr.jp&size=180
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.querySelectorAll("rt").forEach(dom => dom.remove());
+})();
