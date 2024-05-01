43dd3c0b9f3f4df647c8c162f87a3f7543a049ff
どっかのネット民 <115701341+netminn012@users.noreply.github.com>
2024-05-01 10:29:50 +0900

    説明の追加

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Remove-Ruby-for-Aozora-Bunko
 青空文庫からルビを除去するユーザースクリプト
+
+**こんな感じ**
+![](https://netminn012.github.io/assets/demo.webp)
+
+# How to use this script?
+## 1.Tampermonkeyのインストール
+ユーザースクリプトを実行するにはTampermonkeyとかのユーザースクリプトマネージャーが必要です。
+インストールしていないなら[こ↑こ↓](https://www.tampermonkey.net/)から入れてください。
+
+## 2.ユーザースクリプトのインストール
--- userscript.js
+++ userscript.js
@@ -6,11 +6,9 @@
 // @author      Your Name
 // @match       https://www.aozora.gr.jp/*
 // @grant       none
-// @icon　　　　　http://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://www.aozora.gr.jp&size=180
 // ==/UserScript==
 
 (function() {
     'use strict';
-    
     document.querySelectorAll("rt").forEach(dom => dom.remove());
 })();
